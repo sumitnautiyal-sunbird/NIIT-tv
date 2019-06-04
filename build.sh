2e5318d0dd88e35cff5d53271a4e9302fac47ab8
sumitnautiyal-sunbird <47247007+sumitnautiyal-sunbird@users.noreply.github.com>
2019-06-04 14:49:26 +0530

    updated no cache

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ org=stackrouteniit
 name=player
 version=1.13.2
 
-docker build -f ./Dockerfile.Build --build-arg commit_hash=${commit_hash} -t ${org}/${name}:${version}-build . 
+#docker build -f ./Dockerfile.Build --build-arg commit_hash=${commit_hash} -t ${org}/${name}:${version}-build . 
+docker build --no-cache -f ./Dockerfile.Build --build-arg commit_hash=${commit_hash} -t ${org}/${name}:${version}-build . 
 docker run --name=${name}-${version}-build ${org}/${name}:${version}-build
 containerid=$(docker ps -aqf "name=${name}-${version}-build")
 rm -rf ./dist
